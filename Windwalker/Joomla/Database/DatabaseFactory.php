f00e87d720c439660446919137a05335293759e6
Simon Asika <asika32764@gmail.com>
2014-02-21 20:03:20 +0800

    Add Joomla DataMapper & Database

---
+++ Windwalker/Joomla/Database/DatabaseFactory.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Part of Windwalker project. 
+ *
+ * @copyright  Copyright (C) 2011 - 2014 SMS Taiwan, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Windwalker\Joomla\Database;
+
+use JDatabaseDriver as DatabaseDriver;
+
+/**
+ * Class DatabaseFactory
+ */
+abstract class DatabaseFactory
+{
+	/**
+	 * Property db.
+	 *
+	 * @var DatabaseDriver
+	 */
+	protected static $db = null;
+
+	/**
+	 * Property command.
+	 *
+	 * @var  DatabaseCommand
+	 */
+	protected static $command = null;
+
+	/**
+	 * getDbo
+	 *
+	 * @param array $option
+	 * @param bool  $forceNew
+	 *
+	 * @return  DatabaseDriver
+	 */
+	public static function getDbo($option = array(), $forceNew = false)
+	{
+		return \JFactory::getDbo();
+	}
+
+	/**
+	 * setDb
+	 *
+	 * @param   DatabaseDriver $db
+	 *
+	 * @return  void
+	 */
+	public static function setDbo(DatabaseDriver $db)
+	{
+		\JFactory::$database = $db;
+	}
+
+	/**
+	 * getCommand
+	 *
+	 * @param bool $forceNew
+	 *
+	 * @return  DatabaseCommand
+	 */
+	public static function getCommand($forceNew = false)
+	{
+		if (!self::$command || $forceNew)
+		{
+			self::$command = new DatabaseCommand(static::getDbo());
+		}
+
+		return self::$command;
+	}
+
+	/**
+	 * setCommand
+	 *
+	 * @param   DatabaseCommand $command
+	 *
+	 * @return  DatabaseFactory  Return self to support chaining.
+	 */
+	public static function setCommand(DatabaseCommand $command)
+	{
+		self::$command = $command;
+	}
+}
