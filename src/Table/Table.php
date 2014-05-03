15f721d81d2ce10ae500bd96723458bd20bbea40
Simon Asika <asika32764@gmail.com>
2014-05-03 18:21:00 +0800

    Add auto params encode in Table

--- src/Table/Table.php
+++ src/Table/Table.php
@@ -8,12 +8,13 @@
 
 namespace Windwalker\Table;
 
+use JTable;
 use Windwalker\DI\Container;
 
 /**
  * Windwalker Table.
  *
- * @since 1.0
+ * @since 2.0
  */
 class Table extends \JTable
 {
@@ -25,13 +26,35 @@ class Table extends \JTable
 	 * @param   string           $table  Name of the table to model.
 	 * @param   mixed            $key    Name of the primary key field in the table or array of field names that compose the primary key.
 	 * @param   \JDatabaseDriver $db     JDatabaseDriver object.
-	 *
-	 * @since  1.0
 	 */
 	public function __construct($table, $key = 'id', $db = null)
 	{
 		$db = $db ?: Container::getInstance()->get('db');
 
 		parent::__construct($table, $key, $db);
+	}
+
+	/**
+	 * Method to store a row in the database from the JTable instance properties.
+	 * If a primary key value is set the row with that primary key value will be
+	 * updated with the instance property values.  If no primary key value is set
+	 * a new row will be inserted into the database with the properties from the
+	 * JTable instance.
+	 *
+	 * @param   boolean  $updateNulls  True to update fields even if they are null.
+	 *
+	 * @return  boolean  True on success.
+	 */
+	public function store($updateNulls = false)
+	{
+		if (property_exists($this, 'params') && !empty($this->params))
+		{
+			if (is_array($this->params) || is_object($this->params))
+			{
+				$this->params = json_encode($this->params);
+			}
+		}
+
+		return parent::store($updateNulls);
 	}
 }
