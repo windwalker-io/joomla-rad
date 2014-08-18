3f04cc9eed6d31994159a7e0610d4b8e2358d891
Leo <leo.on.the.earth@gmail.com>
2014-08-18 11:22:49 +0800

    fixed user state bug in "Save2newController"

--- src/Controller/Edit/Save2newController.php
+++ src/Controller/Edit/Save2newController.php
@@ -39,7 +39,12 @@ class Save2newController extends SaveController
 	{
 		// Clear the record id and data from the session.
 		$this->releaseEditId($this->context, $this->recordId);
-		$this->app->setUserState($this->context . '.data', null);
+
+		// If save success, clean session.
+		if ($return)
+		{
+			$this->app->setUserState($this->context . '.data', null);
+		}
 
 		// Redirect back to the edit screen.
 		$this->input->set('layout', 'edit');
