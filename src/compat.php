e6acb7e6d4fcb4bb30e34b9e5398c6786c329de8
Simon Asika <asika32764@gmail.com>
2014-09-22 11:09:25 +0800

    Add some compat for Joomla 3.4

--- src/compat.php
+++ src/compat.php
@@ -16,3 +16,58 @@ if (!class_exists('JRegistry'))
 	class JRegistry extends \Joomla\Registry\Registry
 	{}
 }
+
+if (!class_exists('JInput'))
+{
+	/**
+	 * JInput alias.
+	 *
+	 * @since 1.0
+	 */
+	class JInput extends \Joomla\Input\Input
+	{}
+}
+
+if (!class_exists('JFilter'))
+{
+	/**
+	 * JFilter alias.
+	 *
+	 * @since 1.0
+	 */
+	class JFilter extends \Joomla\Filter\InputFilter
+	{}
+}
+
+if (!class_exists('JString'))
+{
+	/**
+	 * JString alias.
+	 *
+	 * @since 1.0
+	 */
+	class JString extends \Joomla\String\String
+	{}
+}
+
+if (!class_exists('Uri'))
+{
+	/**
+	 * Uri alias.
+	 *
+	 * @since 1.0
+	 */
+	class JUri extends \Joomla\Uri\Uri
+	{}
+}
+
+if (!class_exists('JArrayHelper'))
+{
+	/**
+	 * JArrayHelper alias.
+	 *
+	 * @since 1.0
+	 */
+	// class JArrayHelper extends \Joomla\Utilities\ArrayHelper
+	// {}
+}
