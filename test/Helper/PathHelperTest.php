1f687e227d484c4326b00fcd4cdadaba88a6327d
Simon Asika <asika32764@gmail.com>
2015-01-12 12:07:58 +0800

    Reset composer

---
+++ test/Helper/PathHelperTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Part of joomla336 project. 
+ *
+ * @copyright  Copyright (C) 2015 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later;
+ */
+
+namespace Windwalker\Test\Helper;
+
+/**
+ * The PathHelperTest class.
+ * 
+ * @since  {DEPLOY_VERSION}
+ */
+class PathHelperTest extends Ab
+{
+
+}
