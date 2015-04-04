2c0eb92fb2c72f13a37ec3e512921b551c70ed67
skylying <linsutim@gmail.com>
2015-04-04 11:30:32 +0800

    fix name space typo

--- test/Html/HtmlBuilderTest.php
+++ test/Html/HtmlBuilderTest.php
@@ -6,7 +6,7 @@
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
-namespace Windwalker\Test\Helper;
+namespace Windwalker\Test\Html;
 
 use Windwalker\Html\HtmlBuilder;
 
