c21e0ac8c8b5090962e76f69324a58cdb60d9465
leo <leo@bm-sms.com.tw>
2015-04-10 15:03:40 +0800

    added `ContainerHelper` feature to restore original application instance

--- test/DI/ContainerHelper.php
+++ test/DI/ContainerHelper.php
@@ -16,7 +16,14 @@ use Windwalker\DI\Container;
 class ContainerHelper
 {
 	/**
-	 * setApplication
+	 * Property of original Application instance
+	 *
+	 * @var mixed
+	 */
+	protected static $originalApplication = null;
+
+	/**
+	 * Replace original Application instance
 	 *
 	 * @param mixed $app Application instance
 	 *
@@ -26,10 +33,28 @@ class ContainerHelper
 	{
 		$container = Container::getInstance();
 
-		$container->alias('app', 'Application');
-		$container->share('Application', function() use ($app)
+		if (empty($originApplication))
 		{
-			return $app;
-		});
+			static::$originalApplication = $container->get('app');
+		}
+
+		$className = get_class($app);
+
+		$container->alias('app', $className);
+		$container->share($className, $app);
+	}
+
+	/**
+	 * Restore original Application instance
+	 *
+	 * @return  void
+	 */
+	public static function restoreApplication()
+	{
+		$container = Container::getInstance();
+		$className = get_class(static::$originalApplication);
+
+		$container->alias('app', $className);
+		$container->share($className, static::$originalApplication);
 	}
 }
