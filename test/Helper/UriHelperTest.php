dcbce8d51caad6c60284522f390bd9b4feef1c7c
Bing-Sheng Chen <bschen@bm-sms.com.tw>
2015-06-05 17:49:37 +0800

    Generate test skeleton.

---
+++ test/Helper/UriHelperTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Part of joomla-unit-test project. 
+ *
+ * @copyright  Copyright (C) 2015 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later;
+ */
+
+namespace Windwalker\Test\Helper;
+
+/**
+ * The UriHelperTest class.
+ * 
+ * @since  {DEPLOY_VERSION}
+ */
+class UriHelperTest extends \PHPUnit_Framework_TestCase 
+{
+    /**
+     * Test instance.
+     *
+     * @var object
+     */
+    protected $instance;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        $this->instance = null;
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     *
+     * @return void
+     */
+    protected function tearDown()
+    {
+    }
+}
+
