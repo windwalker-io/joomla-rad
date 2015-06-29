ef8b002f5b1bb52334f5d8f876d057e7521f1331
Simon Asika <asika32764@gmail.com>
2015-06-29 10:22:14 +0800

    Remove non-necessary template

--- bundles/GeneratorBundle/Template/component/ihealth/administrator/asset/js/main.js
+++
@@ -1,8 +0,0 @@
-/**
- * @package     Joomla.Administrator
- * @subpackage  {{extension.element.lower}}
- * @copyright   Copyright (C) 2014 Asikart. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-var {{extension.name.cap}} = {};
