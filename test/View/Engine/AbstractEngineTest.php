6b3c9008636ae7b17eb68c1cd22bd9854b451117
leo <leo@bm-sms.com.tw>
2015-08-20 18:10:30 +0800

    added "AbstractEngineTest"

---
+++ test/View/Engine/AbstractEngineTest.php
@@ -0,0 +1,266 @@
+<?php
+/**
+ * Part of Windwalker project Test files.
+ *
+ * @copyright  Copyright (C) 2011 - 2014 SMS Taiwan, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Windwalker\Test\View\Engine;
+
+use Windwalker\DI\Container;
+use Windwalker\Registry\Registry;
+use Windwalker\Test\TestCase\AbstractBaseTestCase;
+use Windwalker\Test\TestHelper;
+use Windwalker\Test\View\Engine\Stub\StubEngine;
+
+/**
+ * Test class of \Windwalker\View\Engine\AbstractEngine
+ *
+ * @since {DEPLOY_VERSION}
+ */
+class AbstractEngineTest extends AbstractBaseTestCase
+{
+	/**
+	 * Method to test __construct().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\View\Engine\AbstractEngine::__construct
+	 */
+	public function test__construct()
+	{
+		$engine = new StubEngine;
+
+		$this->assertEquals(new Registry(array()), TestHelper::getValue($engine, 'config'));
+		$this->assertSame(Container::getInstance(), TestHelper::getValue($engine, 'container'));
+		$this->assertEquals(new \SplPriorityQueue, TestHelper::getValue($engine, 'paths'));
+
+		$paths = new \SplPriorityQueue;
+		$container = new Container;
+
+		$paths->insert(__DIR__ . '/tmpl', 0);
+
+		$engine = new StubEngine(array('foo' => 'bar'), $container, $paths);
+
+		$this->assertEquals(new Registry(array('foo' => 'bar')), TestHelper::getValue($engine, 'config'));
+		$this->assertSame($container, TestHelper::getValue($engine, 'container'));
+		$this->assertEquals($paths, TestHelper::getValue($engine, 'paths'));
+	}
+
+	/**
+	 * Method to test render().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\View\Engine\AbstractEngine::render
+	 */
+	public function testRender()
+	{
+		$engine = new StubEngine;
+		$paths = new \SplPriorityQueue;
+
+		$paths->insert(__DIR__ . '/tmpl', 0);
+
+		$engine->setPaths($paths);
+
+		$result = $engine->render('default');
+
+		$this->assertEquals(realpath(__DIR__ . '/tmpl/default.php'), $result);
+	}
+
+	/**
+	 * Method to test loadTemplate().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\View\Engine\AbstractEngine::loadTemplate
+	 */
+	public function testLoadTemplate()
+	{
+		$engine = new StubEngine;
+		$paths = new \SplPriorityQueue;
+
+		$paths->insert(__DIR__ . '/tmpl', 0);
+
+		$engine->setLayout('default');
+		$engine->setPaths($paths);
+
+		$result = $engine->loadTemplate('foo');
+
+		$this->assertEquals(realpath(__DIR__ . '/tmpl/default_foo.php'), $result);
+	}
+
+	/**
+	 * Method to test loadTemplate().
+	 *
+	 * @return void
+	 *
+	 * @expectedException \Exception
+	 * @expectedExceptionMessage JLIB_APPLICATION_ERROR_LAYOUTFILE_NOT_FOUND
+	 *
+	 * @covers Windwalker\View\Engine\AbstractEngine::loadTemplate
+	 */
+	public function testLoadTemplateException()
+	{
+		$engine = new StubEngine;
+
+		$engine->loadTemplate();
+	}
+
+	/**
+	 * Method to test escape().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\View\Engine\AbstractEngine::escape
+	 */
+	public function testEscape()
+	{
+		$engine = new StubEngine;
+
+		$this->assertEquals('#&amp;&lt;&gt;&quot;\'', $engine->escape('#&<>"\''));
+	}
+
+	/**
+	 * Method to test getLayoutTemplate().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\View\Engine\AbstractEngine::getLayoutTemplate
+	 */
+	public function testGetLayoutTemplate()
+	{
+		$engine = new StubEngine;
+		$layoutTemplate = 'template';
+
+		TestHelper::setValue($engine, 'layoutTemplate', $layoutTemplate);
+
+		$this->assertEquals($layoutTemplate, $engine->getLayoutTemplate());
+	}
+
+	/**
+	 * Method to test getPath().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\View\Engine\AbstractEngine::getPath
+	 */
+	public function testGetPath()
+	{
+		$engine = new StubEngine;
+		$paths = new \SplPriorityQueue;
+
+		$paths->insert(__DIR__ . '/tmpl', 0);
+
+		$engine->setLayout('default');
+		$engine->setPaths($paths);
+
+		$result = $engine->loadTemplate('foo');
+
+		$this->assertStringSafeEquals(realpath(__DIR__ . '/tmpl/default_foo.php'), $result);
+	}
+
+	/**
+	 * Method to test getPaths().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\View\Engine\AbstractEngine::getPaths
+	 */
+	public function testGetPaths()
+	{
+		$engine = new StubEngine;
+
+		$this->assertSame(TestHelper::getValue($engine, 'paths'), $engine->getPaths());
+	}
+
+	/**
+	 * Method to test setPaths().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\View\Engine\AbstractEngine::setPaths
+	 */
+	public function testSetPaths()
+	{
+		$engine = new StubEngine;
+		$paths = new \SplPriorityQueue;
+
+		$paths->insert(__DIR__ . '/tmpl', 0);
+
+		$engine->setPaths($paths);
+
+		$this->assertSame($paths, TestHelper::getValue($engine, 'paths'));
+	}
+
+	/**
+	 * Method to test getLayout().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\View\Engine\AbstractEngine::getLayout
+	 */
+	public function testGetLayout()
+	{
+		$engine = new StubEngine;
+
+		$this->assertSame(TestHelper::getValue($engine, 'layout'), $engine->getLayout());
+	}
+
+	/**
+	 * Method to test setLayout().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\View\Engine\AbstractEngine::setLayout
+	 */
+	public function testSetLayout()
+	{
+		$engine = new StubEngine;
+
+		$engine->setLayout('default');
+		$this->assertEquals('default', TestHelper::getValue($engine, 'layout'));
+
+		$engine->setLayout('template:default');
+		$this->assertEquals('default', TestHelper::getValue($engine, 'layout'));
+		$this->assertEquals('template', TestHelper::getValue($engine, 'layoutTemplate'));
+	}
+
+	/**
+	 * Method to test getContainer().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\View\Engine\AbstractEngine::getContainer
+	 */
+	public function testGetContainer()
+	{
+		$engine = new StubEngine;
+
+		$this->assertSame(Container::getInstance(), $engine->getContainer());
+
+		$container = new Container;
+
+		$engine = new StubEngine(array(), $container);
+
+		$this->assertSame($container, $engine->getContainer());
+	}
+
+	/**
+	 * Method to test setContainer().
+	 *
+	 * @return void
+	 *
+	 * @covers Windwalker\View\Engine\AbstractEngine::setContainer
+	 */
+	public function testSetContainer()
+	{
+		$engine = new StubEngine;
+		$container = new Container;
+
+		$engine->setContainer($container);
+
+		$this->assertSame($container, TestHelper::getValue($engine, 'container'));
+	}
+}
---
+++ test/View/Engine/Stub/StubEngine.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Part of Windwalker project.
+ *
+ * @copyright  Copyright (C) 2015 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later;
+ */
+
+namespace Windwalker\Test\View\Engine\Stub;
+
+use Windwalker\Data\Data;
+use Windwalker\View\Engine\AbstractEngine;
+
+/**
+ * The StubEngine class.
+ * 
+ * @since  {DEPLOY_VERSION}
+ */
+class StubEngine extends AbstractEngine
+{
+	/**
+	 * Execute a template and return to loadTemplate() method.
+	 *
+	 * @param string $templateFile The template file name.
+	 * @param array  $data         The data to push into layout.
+	 *
+	 * @return  mixed
+	 */
+	protected function execute($templateFile, $data = null)
+	{
+		$data = new Data($data);
+
+		return $templateFile . ($data->foo ? ' ' . $data->foo : '');
+	}
+}
