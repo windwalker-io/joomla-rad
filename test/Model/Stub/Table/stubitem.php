dc5902c4efb2d29cb49333acaeba5bd80e75e662
Bing-Sheng Chen <bschen@bm-sms.com.tw>
2015-09-01 16:33:27 +0800

    Add ItemModelTest.

---
+++ test/Model/Stub/Table/stubitem.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Part of Windwalker project.
+ *
+ * @copyright  Copyright (C) 2015 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later;
+ */
+
+use Windwalker\Table\Table;
+
+/**
+ * The WindwalkerTableStubItem class.
+ * 
+ * @since  {DEPLOY_VERSION}
+ */
+class WindwalkerTableStubitem extends Table
+{
+	/**
+	 * Class constructor
+	 *
+	 * return  void
+	 */
+	public function __construct()
+	{
+		parent::__construct('#__test_table');
+	}
+}
