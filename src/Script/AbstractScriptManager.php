1efba5006712abae77dd817e95da419b52829e55
Simon Asika <asika32764@gmail.com>
2016-02-02 00:50:21 +0800

    AbstractScriptManager

---
+++ src/Script/AbstractScriptManager.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Part of rad project.
+ *
+ * @copyright  Copyright (C) 2016 {ORGANIZATION}. All rights reserved.
+ * @license    GNU General Public License version 2 or later.
+ */
+
+namespace Windwalker\Script;
+
+use Windwalker\DI\Container;
+use Windwalker\Helper\AssetHelper;
+
+/**
+ * The AbstractScriptManager class.
+ *
+ * @since  {DEPLOY_VERSION}
+ */
+class AbstractScriptManager
+{
+	/**
+	 * Property inited.
+	 *
+	 * @var  array
+	 */
+	protected static $inited = array();
+
+	/**
+	 * Property asset.
+	 *
+	 * @var  AssetHelper
+	 */
+	protected static $assets;
+
+	/**
+	 * inited
+	 *
+	 * @param   string $name
+	 * @param   mixed  $data
+	 *
+	 * @return bool
+	 */
+	protected static function inited($name, $data = null)
+	{
+		$id = static::getInitedId($data);
+
+		$class = get_called_class();
+
+		if (!isset(static::$inited[$class][$name][$id]))
+		{
+			static::$inited[$class][$name][$id] = true;
+
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * getInitedId
+	 *
+	 * @param   mixed  $data
+	 *
+	 * @return  string
+	 */
+	protected static function getInitedId($data)
+	{
+		return sha1(serialize($data));
+	}
+
+	/**
+	 * Method to get property Asset
+	 *
+	 * @param string $option
+	 *
+	 * @return AssetHelper
+	 */
+	public static function getAsset($option = 'windwalker')
+	{
+		if (!empty(static::$assets[$option]))
+		{
+			return static::$assets[$option];
+		}
+
+		$container = static::getContainer($option);
+
+		if ($container->exists('helper.asset'))
+		{
+			$asset = $container->get('helper.asset');
+		}
+		else
+		{
+			$asset = new AssetHelper($option);
+		}
+
+		return static::$assets[$option] = $asset;
+	}
+
+	/**
+	 * Method to set property asset
+	 *
+	 * @param   string      $option
+	 * @param   AssetHelper $asset
+	 *
+	 * @return  void
+	 */
+	public static function setAsset($option, AssetHelper $asset)
+	{
+		static::$assets[$option] = $asset;
+	}
+
+	/**
+	 * getContainer
+	 *
+	 * @param string $option
+	 *
+	 * @return Container
+	 */
+	protected static function getContainer($option = 'windwalker')
+	{
+		return Container::getInstance($option);
+	}
+}
