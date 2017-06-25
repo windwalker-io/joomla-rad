3f0480a3b3e834660df2182f0e10b44bf7ca1f1f
Simon Asika <asika32764@gmail.com>
2017-06-25 13:34:57 +0800

    Prepare new install

---
+++ bin/dirs.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Part of Component Ezset files.
+ *
+ * @copyright   Copyright (C) 2017 Asikart.co. All rights reserved.
+ * @license     GNU General Public License version 2 or later.
+ */
+
+const _JEXEC = 1;
+defined('_JEXEC') or die;
+
+return array(
+	'libraries/windwalker' => '.',
+	'media/windwalker' => 'asset',
+);
