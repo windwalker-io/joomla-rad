2070910ef060a5b74c171f1db595a67895032747
Simon Asika <asika32764@gmail.com>
2017-07-05 22:27:10 +0800

    Add AjaxController

---
+++ src/Controller/Ajax/AbstractAjaxController.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * Part of Windwalker project.
+ *
+ * @copyright  Copyright (C) 2016 LYRASOFT. All rights reserved.
+ * @license    GNU General Public License version 2 or later.
+ */
+
+namespace Windwalker\Controller\Ajax;
+
+use Windwalker\Controller\Controller;
+
+/**
+ * The AbstractAjaxController class.
+ *
+ * @since  2.1.12
+ */
+abstract class AbstractAjaxController extends Controller
+{
+	/**
+	 * Property contentType.
+	 *
+	 * @var  string
+	 */
+	protected $contentType = 'application/json';
+
+	/**
+	 * Property format.
+	 *
+	 * @var  string
+	 */
+	protected $format = 'json';
+
+	/**
+	 * Method to run this controller.
+	 *
+	 * @return  mixed
+	 */
+	protected function doExecute()
+	{
+		try
+		{
+			$data = $this->doAjax();
+
+			if ($this->format === 'json')
+			{
+				$buffer = new \JResponseJson($data, null);
+			}
+			else
+			{
+				$buffer = $data;
+			}
+		}
+		catch (\Exception $e)
+		{
+			$msg = null;
+
+			if (JDEBUG)
+			{
+				$msg = array(
+					'exception' => get_class($e),
+					'file'      => $e->getFile(),
+					'line'      => $e->getLine(),
+					'trace'     => explode("\n", $e->getTraceAsString())
+				);
+			}
+
+			$buffer = new \JResponseJson($msg, $e->getMessage(), true);
+
+			header('HTTP/1.1 ' . $e->getCode() . ' ' . $e->getMessage());
+		}
+
+		header('Content-Type: ' . $this->contentType);
+
+		echo $buffer;
+		die;
+	}
+
+	/**
+	 * Execute ajax.
+	 *
+	 * @return  mixed
+	 */
+	abstract protected function doAjax();
+
+	/**
+	 * Use JSON response.
+	 *
+	 * @return  static
+	 */
+	public function useJson()
+	{
+		$this->setContentType('application/json')
+			->setFormat('json');
+
+		return $this;
+	}
+
+	/**
+	 * Use HTML response.
+	 *
+	 * @return  static
+	 */
+	public function useHtml()
+	{
+		$this->setContentType('text/html')
+			->setFormat('html');
+
+		return $this;
+	}
+
+	/**
+	 * Method to get property ContentType
+	 *
+	 * @return  string
+	 */
+	public function getContentType()
+	{
+		return $this->contentType;
+	}
+
+	/**
+	 * Method to set property contentType
+	 *
+	 * @param   string $contentType
+	 *
+	 * @return  static  Return self to support chaining.
+	 */
+	public function setContentType($contentType)
+	{
+		$this->contentType = $contentType;
+
+		return $this;
+	}
+
+	/**
+	 * Method to get property Format
+	 *
+	 * @return  string
+	 */
+	public function getFormat()
+	{
+		return $this->format;
+	}
+
+	/**
+	 * Method to set property format
+	 *
+	 * @param   string $format
+	 *
+	 * @return  static  Return self to support chaining.
+	 */
+	public function setFormat($format)
+	{
+		$this->format = $format;
+
+		return $this;
+	}
+}
