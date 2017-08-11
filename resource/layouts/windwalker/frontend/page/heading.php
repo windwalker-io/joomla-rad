eee31819b6b7a55756a60b1d419e460e3a2c4e2c
Simon Asika <asika32764@gmail.com>
2017-08-11 23:31:23 +0800

    Modify heading layout

--- resource/layouts/windwalker/frontend/page/heading.php
+++ resource/layouts/windwalker/frontend/page/heading.php
@@ -6,25 +6,25 @@
  * @license    GNU General Public License version 2 or later.
  */
 
-use Windwalker\Data\Data;
-
-$data = new Data($displayData);
-
 /**
- * @var $this  \Windwalker\View\Engine\PhpEngine
+ * @var $this      \Windwalker\View\Engine\PhpEngine
+ * @var $params    \Joomla\Registry\Registry
+ * @var $category  \Windwalker\Data\Data
  */
+
+extract($displayData, EXTR_OVERWRITE);
 ?>
-<?php if ($data->params->get('show_page_heading')) : ?>
+<?php if ($params->get('show_page_heading')) : ?>
 	<h1>
-		<?php echo $this->escape($data->params->get('page_heading')); ?>
+		<?php echo $this->escape($params->get('page_heading')); ?>
 	</h1>
 <?php endif; ?>
 
-<?php if ($data->params->get('show_category_title') or $data->params->get('page_subheading')) : ?>
+<?php if (($params->get('show_category_title') || $params->get('page_subheading')) && isset($category)) : ?>
 	<h2>
-		<?php echo $this->escape($data->params->get('page_subheading')); ?>
-		<?php if ($data->params->get('show_category_title')) : ?>
-			<span class="subheading-category"><?php echo $this->escape($data->category->title); ?></span>
+		<?php echo $this->escape($params->get('page_subheading')); ?>
+		<?php if ($params->get('show_category_title')) : ?>
+			<span class="subheading-category"><?php echo $this->escape($category->title); ?></span>
 		<?php endif; ?>
 	</h2>
 <?php endif; ?>
