d89c09e00d5736ff380bec765a6ec0c71e434280
Simon Asika <asika32764@gmail.com>
2018-01-28 21:42:07 +0800

    Fix test dir

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -30,7 +30,7 @@ if (file_exists(dirname(__DIR__) . '/../../defines.php'))
 
 if (!defined('_JDEFINES'))
 {
-	define('JPATH_BASE', realpath(isset($_ENV['JOOMLA_ROOT']) ? $_ENV['JOOMLA_ROOT'] : __DIR__ . '/../..'));
+	define('JPATH_BASE', realpath(isset($_ENV['JOOMLA_ROOT']) ? $_ENV['JOOMLA_ROOT'] : __DIR__ . '/../../..'));
 	require_once JPATH_BASE . '/includes/defines.php';
 }
 
